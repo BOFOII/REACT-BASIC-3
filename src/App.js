43bdeab1cf533f049de65729b0b4a34aef725c8d
XarraXPMMP <imzalpha2@gmail.com>
2021-10-05 13:30:21 +0700

    Added Devschool pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Premium from './pages/Premium.js';
+import DevSchool from './pages/DevSchool';
 
 class App extends Component {
   render() {
@@ -19,7 +20,7 @@ class App extends Component {
           </nav>
           <Route path="/" exact component={ Home }></Route>
           <Route path="/premium" component={ Premium }></Route>
-          <Route path="/devschool" component={ () => <h4>Devschool</h4> }></Route>
+          <Route path="/devschool" component={ DevSchool }></Route>
         </div>
       </Router>
     );
